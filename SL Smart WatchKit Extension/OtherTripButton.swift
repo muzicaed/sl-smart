9da8a798a600922392570bb6cdcde2212806b8ec
Mikael Hellman <mikael.hellman@gmail.com>
2015-12-03 00:11:53 +0100

    Icons + Watch UI

--- SL Smart WatchKit Extension/OtherTripButton.swift
+++ SL Smart WatchKit Extension/OtherTripButton.swift
@@ -7,3 +7,8 @@
 //
 
 import Foundation
+import WatchKit
+
+class OtherTripButton: WKInterfaceButton {
+
+}
--- SL Smart WatchKit Extension/SmartTripIC.swift
+++ SL Smart WatchKit Extension/SmartTripIC.swift
@@ -1,5 +1,5 @@
 //
-//  InterfaceController.swift
+//  SmartTripIC.swift
 //  SL Smart WatchKit Extension
 //
 //  Created by Mikael Hellman on 2015-11-20.
@@ -8,24 +8,59 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
-class SmartTripIC: WKInterfaceController {
-
+class SmartTripIC: WKInterfaceController, WCSessionDelegate {
+    
+    @IBOutlet var titleLabel: WKInterfaceLabel!
+    @IBOutlet var departureTimeLabel: WKInterfaceLabel!
+    @IBOutlet var originLabel: WKInterfaceLabel!
+    @IBOutlet var destinationLabel: WKInterfaceLabel!
+    @IBOutlet var travelTypeIconWrapper: WKInterfaceGroup!
+    
+    var session : WCSession?
+    
+    /**
+     * On load.
+     */
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
-        // Configure interface objects here.
     }
-
+    
+    /**
+     * About to show on screen.
+     */
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        if (WCSession.isSupported()) {
+            session = WCSession.defaultSession()
+            session!.delegate = self
+            session!.activateSession()
+            session!.sendMessage(["action": "requestRoutineTrips"],
+                replyHandler: requestRoutineTripsHandler,
+                errorHandler: messageErrorHandler)
+        }
     }
-
+    
+    /**
+     * Did disapear from screen.
+     */
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    /**
+     * Handle reply for a "requestRoutineTrips" message.
+     */
+    func requestRoutineTripsHandler(reply: [String: AnyObject]) {
+        print(reply["test"])
+    }
+    
+    /**
+     * Handles any session send messages errors.
+     */
+    func messageErrorHandler(error: NSError) {
+        fatalError("Message error: \(error)")
+    }
 }
