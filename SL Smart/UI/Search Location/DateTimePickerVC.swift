c05ac164b2440ccbdec2f8faeb525ee8a330d83f
Mikael Hellman <mikael.hellman@gmail.com>
2015-12-09 12:10:33 +0100

    More on search trip

---
+++ SL Smart/UI/Search Location/DateTimePickerVC.swift
@@ -0,0 +1,27 @@
+//
+//  DateTimePickerVC.swift
+//  SL Smart
+//
+//  Created by Mikael Hellman on 2015-12-09.
+//  Copyright © 2015 Mikael Hellman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DateTimePickerVC: UIViewController {
+
+  
+  /**
+   * On close button tap
+   */
+  @IBAction func onCloseButtonTap(sender: AnyObject) {
+  }
+  
+  /**
+   * On pick time button tap
+   */
+  @IBAction func onPickTimeButtonTap(sender: AnyObject) {
+  }
+}
+
