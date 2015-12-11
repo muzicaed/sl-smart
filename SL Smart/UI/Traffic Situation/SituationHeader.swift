43c7b18c9144f12b9bf392ce6d12a4dd2c63281b
Mikael Hellman <mikael.hellman@gmail.com>
2015-12-11 12:17:44 +0100

    UI Tweaks for Traffic Situation

--- SL Smart/UI/Traffic Situation/SituationHeader.swift
+++ SL Smart/UI/Traffic Situation/SituationHeader.swift
@@ -11,7 +11,7 @@ import UIKit
 import ResStockholmApiKit
 
 class SituationHeader: UITableViewCell {
-
+  
   @IBOutlet weak var title: UILabel!
   @IBOutlet weak var statusIcon: UIImageView!
   @IBOutlet weak var typeIcon: UIImageView!
@@ -23,7 +23,19 @@ class SituationHeader: UITableViewCell {
     title.text = group.name
     statusIcon.image = UIImage(named: group.statusIcon)
     typeIcon.image = UIImage(named: group.type)
+    setBackgroundColor(group.statusIcon)
   }
   
+  // MARK: Private
   
+  /**
+  * Set background color based on type of situation
+  */
+  private func setBackgroundColor(status: String) {
+    if status == "EventMajor" {
+      title.textColor = UIColor(red: 249/255, green: 93/255, blue: 89/255, alpha: 1.0)
+    } else if status == "EventMinor" {
+      title.textColor = UIColor(red: 235/255, green: 151/255, blue: 89/255, alpha: 1.0)
+    }
+  }
 }
