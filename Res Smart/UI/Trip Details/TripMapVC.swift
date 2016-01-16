cf45b02f0ca58097537a699830818dfc6420f95c
Mikael Hellman <mikael.hellman@gmail.com>
2016-01-16 23:03:21 +0100

    Basic map features

---
+++ Res Smart/UI/Trip Details/TripMapVC.swift
@@ -0,0 +1,61 @@
+//
+//  TripMapVC.swift
+//  Res Smart
+//
+//  Created by Mikael Hellman on 2016-01-16.
+//  Copyright © 2016 Mikael Hellman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+import ResStockholmApiKit
+
+class TripMapVC: UIViewController {
+  
+  @IBOutlet weak var mapView: MKMapView!
+  
+  /**
+   * View did load
+   */
+  override func viewDidLoad() {
+    
+  }
+  
+  /**
+   * User tapped close
+   */
+  @IBAction func onCloseTap(sender: AnyObject) {
+    presentingViewController?.dismissViewControllerAnimated(true, completion: {})
+  }
+  
+  /**
+   * Map type segment changed
+   */
+  @IBAction func onSegmentChanged(sender: UISegmentedControl) {
+    switch sender.selectedSegmentIndex {
+    case 0:
+      mapView.mapType = MKMapType.Standard
+    case 1:
+      mapView.mapType = MKMapType.Satellite
+    case 2:
+      mapView.mapType = MKMapType.Hybrid
+    default: break
+    }
+  }
+  
+  /**
+   * User tapped my position
+   */
+  @IBAction func myPositionTap(sender: AnyObject) {
+    if mapView.showsUserLocation {
+      if mapView.userLocationVisible {
+        mapView.showsUserLocation = false
+      } else {
+        mapView.setCenterCoordinate(mapView.userLocation.coordinate, animated: true)
+      }
+    } else {
+      mapView.showsUserLocation = true
+    }
+  }
+}
