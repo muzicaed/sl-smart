b63a8fe4b098d07d471bee680d87ef3450a75129
Mikael Hellman <mikael.hellman@gmail.com>
2016-01-19 11:04:03 +0100

    More real time navigation foundation

---
+++ Res Smart/UI/Real time/RealTimeVC.swift
@@ -0,0 +1,15 @@
+//
+//  RealtimeVC.swift
+//  Res Smart
+//
+//  Created by Mikael Hellman on 2016-01-19.
+//  Copyright © 2016 Mikael Hellman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RealTimeVC: UITableViewController {
+
+
+)
