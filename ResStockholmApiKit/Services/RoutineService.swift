22e6ef53bdb3fe2882fa1adaefbfe983bae54b09
Mikael Hellman <mikael.hellman@gmail.com>
2016-02-01 08:45:28 +0100

    Removed test code

--- ResStockholmApiKit/Services/RoutineService.swift
+++ ResStockholmApiKit/Services/RoutineService.swift
@@ -220,7 +220,7 @@ public class RoutineService {
     for (index, routine) in routineTrips.enumerate() {
       if index == 0 && routine.isSmartSuggestion && routine.score > 30 && routineTrips.count > 1 {
         filteredList.append(routineTrips[0])
-      } else {//if !routine.isSmartSuggestion {
+      } else if !routine.isSmartSuggestion {
         filteredList.append(routine)
       }
     }
