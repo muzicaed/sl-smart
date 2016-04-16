31b1a7547fe206512861047beb15677c09e6fa58
Mikael Hellman <mikael.hellman@gmail.com>
2016-04-16 11:02:23 +0200

    Added test foundation for ResStockholmApiKit

---
+++ ResStockholmApiKitTests/SearchCriterionStoreTests.swift
@@ -0,0 +1,18 @@
+//
+//  SearchCriterionStoreTests.swift
+//  Res Smart
+//
+//  Created by Mikael Hellman on 2016-04-16.
+//  Copyright © 2016 Mikael Hellman. All rights reserved.
+//
+
+import XCTest
+@testable import ResStockholmApiKit
+
+
+class SearchCriterionStoreTests: XCTestCase {
+  
+  func testFunc() {
+    XCTAssertTrue(true, "True is not True")
+  }
+}
