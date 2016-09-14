fdaf2d90a6c54b24de929aee2106d6ec808fd61b
Mikael Hellman <mikael.hellman@gmail.com>
2016-09-14 09:53:33 +0200

    Map tweaks

--- Res Smart/UI/Map/TripMapVC.swift
+++ Res Smart/UI/Map/TripMapVC.swift
@@ -16,6 +16,8 @@ class TripMapVC: UIViewController, MKMapViewDelegate {
   @IBOutlet weak var mapView: MKMapView!
   var trip: Trip?
   var routePolylineViews = [MKPolylineView]()
+  var smallPins = [SmallPin]()
+  var isSmallPinsVisible = true
   
   /**
    * View did load
@@ -66,7 +68,6 @@ class TripMapVC: UIViewController, MKMapViewDelegate {
    * Annotation views
    */
   func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-    
     var reuseId: String? = nil
     var image: UIImage? = nil
     var bgColor = UIColor.clearColor()
@@ -79,13 +80,14 @@ class TripMapVC: UIViewController, MKMapViewDelegate {
         image = UIImage(named: name)!
         bgColor = UIColor(white: 1.0, alpha: 0.9)
         isShadow = true
+        reuseId = name
       }
-      zIndex = 1
+      zIndex = 2
       
     } else if annotation.isKindOfClass(DestinationPin) {
       reuseId = "destination-dot"
       image = UIImage(named: "MapDestinationDot")!
-      zIndex = 1
+      zIndex = 3
       
     } else if annotation.isKindOfClass(SmallPin) {
       let pinIcon = annotation as! SmallPin
@@ -93,7 +95,9 @@ class TripMapVC: UIViewController, MKMapViewDelegate {
         image = UIImage(named: name)!
         bgColor = UIColor(white: 1.0, alpha: 0.9)
         isShadow = true
+        reuseId = name
       }
+      smallPins.append(pinIcon)
       zIndex = 1
       
     } else {
@@ -137,146 +141,159 @@ class TripMapVC: UIViewController, MKMapViewDelegate {
     }
     return MKOverlayRenderer()
   }
-
   
-  // MARK: Private
+  func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+    if (mapView.region.span.latitudeDelta > 0.25) {
+      if isSmallPinsVisible {
+        mapView.removeAnnotations(smallPins)
+        isSmallPinsVisible = false
+      }
+    } else {
+      if !isSmallPinsVisible {
+        isSmallPinsVisible = true
+        mapView.addAnnotations(smallPins)
+      }
+    }
+  }
   
-  /**
-   * Loads map route
-   */
-  private func loadRoute() {
-    if let trip = trip {
-      var allCoords = [CLLocationCoordinate2D]()
-      for segment in trip.allTripSegments {
-        let coords = plotRoute(segment)
-        createOverlays(coords, segment: segment)
-        allCoords += coords
+    // MARK: Private
+    
+    /**
+     * Loads map route
+     */
+    private func loadRoute() {
+      if let trip = trip {
+        var allCoords = [CLLocationCoordinate2D]()
+        for segment in trip.allTripSegments {
+          let coords = plotRoute(segment)
+          createOverlays(coords, segment: segment)
+          allCoords += coords
+        }
+        
+        setMapViewport(allCoords)
+      }
+    }
+    
+    /**
+     * Plots the coordinates for the route.
+     */
+    
+    private func plotRoute(segment: TripSegment) -> [CLLocationCoordinate2D] {
+      var coords = [CLLocationCoordinate2D]()
+      if segment.stops.count == 0 {
+        coords.append(segment.origin.location.coordinate)
+        coords.append(segment.destination.location.coordinate)
+      } else {
+        for stop in segment.stops {
+          coords.append(stop.location.coordinate)
+        }
+      }
+      return coords
+    }
+    
+    /**
+     * Plots a trip segment route on map and
+     * creates overlay icons.
+     */
+    private func createOverlays(coordinates: [CLLocationCoordinate2D], segment: TripSegment) {
+      var newCoordinates = coordinates
+      let polyline = RoutePolyline(coordinates: &newCoordinates, count: newCoordinates.count)
+      polyline.segment = segment
+      mapView.addOverlay(polyline)
+      
+      createStopPins(segment)
+      createLocationPins(segment, coordinates: newCoordinates)
+    }
+    
+    /**
+     * Create location pins for each segment
+     */
+    private func createLocationPins(segment: TripSegment, coordinates: [CLLocationCoordinate2D]) {
+      let originCoord = (segment.stops.count == 0) ? segment.origin.location.coordinate : segment.stops.first!.location.coordinate
+      let destCoord = (segment.stops.count == 0) ? segment.destination.location.coordinate : segment.stops.last!.location.coordinate
+      if segment == trip?.tripSegments.first! {
+        let pin = BigPin()
+        pin.coordinate = originCoord
+        pin.title = "Start: " + segment.origin.name
+        pin.subtitle = "Avgång: " + DateUtils.dateAsTimeString(segment.departureDateTime)
+        pin.imageName = segment.type.rawValue
+        mapView.addAnnotation(pin)
+        mapView.selectAnnotation(pin, animated: true)
+        return
+      } else if segment == trip?.tripSegments.last! {
+        let pin = BigPin()
+        pin.coordinate = originCoord
+        pin.title = segment.origin.name
+        pin.subtitle = "Avgång: " + DateUtils.dateAsTimeString(segment.departureDateTime)
+        pin.imageName = segment.type.rawValue
+        mapView.addAnnotation(pin)
+        
+        let destPin = DestinationPin()
+        destPin.coordinate = destCoord
+        destPin.title = "Destination: " + segment.destination.name
+        destPin.subtitle = "Framme: " + DateUtils.dateAsTimeString(segment.arrivalDateTime)
+        mapView.addAnnotation(destPin)
+        return
       }
       
-      setMapViewport(allCoords)
-    }
-  }
-  
-  /**
-   * Plots the coordinates for the route.
-   */
-  
-  private func plotRoute(segment: TripSegment) -> [CLLocationCoordinate2D] {
-    var coords = [CLLocationCoordinate2D]()
-    if segment.stops.count == 0 {
-      coords.append(segment.origin.location.coordinate)
-      coords.append(segment.destination.location.coordinate)
-    } else {
-      for stop in segment.stops {
-        coords.append(stop.location.coordinate)
-      }
-    }
-    return coords
-  }
-  
-  /**
-   * Plots a trip segment route on map and
-   * creates overlay icons.
-   */
-  private func createOverlays(coordinates: [CLLocationCoordinate2D], segment: TripSegment) {
-    var newCoordinates = coordinates
-    let polyline = RoutePolyline(coordinates: &newCoordinates, count: newCoordinates.count)
-    polyline.segment = segment
-    mapView.addOverlay(polyline)
-    
-    createStopPins(segment)
-    createLocationPins(segment, coordinates: newCoordinates)
-  }
-  
-  /**
-   * Create location pins for each segment
-   */
-  private func createLocationPins(segment: TripSegment, coordinates: [CLLocationCoordinate2D]) {
-    let originCoord = (segment.stops.count == 0) ? segment.origin.location.coordinate : segment.stops.first!.location.coordinate
-    let destCoord = (segment.stops.count == 0) ? segment.destination.location.coordinate : segment.stops.last!.location.coordinate
-    if segment == trip?.tripSegments.first! {
-      let pin = BigPin()
-      pin.coordinate = originCoord
-      pin.title = "Start: " + segment.origin.name
-      pin.subtitle = "Avgång: " + DateUtils.dateAsTimeString(segment.departureDateTime)
-      pin.imageName = segment.type.rawValue
-      mapView.addAnnotation(pin)
-      mapView.selectAnnotation(pin, animated: true)
-      return
-    } else if segment == trip?.tripSegments.last! {
       let pin = BigPin()
       pin.coordinate = originCoord
       pin.title = segment.origin.name
       pin.subtitle = "Avgång: " + DateUtils.dateAsTimeString(segment.departureDateTime)
       pin.imageName = segment.type.rawValue
       mapView.addAnnotation(pin)
-      
-      let destPin = DestinationPin()
-      destPin.coordinate = destCoord
-      destPin.title = "Destination: " + segment.destination.name
-      destPin.subtitle = "Framme: " + DateUtils.dateAsTimeString(segment.arrivalDateTime)
-      mapView.addAnnotation(destPin)
-      return
     }
     
-    let pin = BigPin()
-    pin.coordinate = originCoord
-    pin.title = segment.origin.name
-    pin.subtitle = "Avgång: " + DateUtils.dateAsTimeString(segment.departureDateTime)
-    pin.imageName = segment.type.rawValue
-    mapView.addAnnotation(pin)
-  }
-  
-  /**
-   * Create location pins for each stop
-   */
-  private func createStopPins(segment: TripSegment) {
-    for stop in segment.stops {
-      if stop.id != segment.stops.first!.id && stop.id != segment.stops.last!.id {
-        let pin = SmallPin()
-        pin.coordinate = stop.location.coordinate
-        pin.title = stop.name
-        if let depDate = stop.depDate {
-          pin.subtitle = "Avgång: " + DateUtils.dateAsTimeString(depDate)
+    /**
+     * Create location pins for each stop
+     */
+    private func createStopPins(segment: TripSegment) {
+      for stop in segment.stops {
+        if stop.id != segment.stops.first!.id && stop.id != segment.stops.last!.id {
+          let pin = SmallPin()
+          pin.coordinate = stop.location.coordinate
+          pin.title = stop.name
+          if let depDate = stop.depDate {
+            pin.subtitle = "Avgång: " + DateUtils.dateAsTimeString(depDate)
+          }
+          pin.imageName = segment.type.rawValue + "-SMALL"
+          mapView.addAnnotation(pin)
         }
-        pin.imageName = segment.type.rawValue + "-SMALL"
-        mapView.addAnnotation(pin)
       }
     }
-  }
-  
-  /**
-   * Centers and zooms map
-   */
-  private func setMapViewport(coordinates: [CLLocationCoordinate2D]) {
-    var newCoordinates = coordinates
-    let allPolyline = MKPolyline(coordinates: &newCoordinates, count: newCoordinates.count)
-    self.mapView.setVisibleMapRect(
-      self.mapView.mapRectThatFits(allPolyline.boundingMapRect),
-      edgePadding: UIEdgeInsets(top: 100, left: 50, bottom: 100, right: 50),
-      animated: false)
-  }
-  
-  /**
-   * Find coordinate between two coordinates.
-   */
-  private func findCenterCoordinate(
-    coord1: CLLocationCoordinate2D, coord2: CLLocationCoordinate2D) -> CLLocationCoordinate2D {
     
-    let lon1 = Double(coord1.longitude) * M_PI / 180
-    let lon2 = Double(coord2.longitude) * M_PI / 180
+    /**
+     * Centers and zooms map
+     */
+    private func setMapViewport(coordinates: [CLLocationCoordinate2D]) {
+      var newCoordinates = coordinates
+      let allPolyline = MKPolyline(coordinates: &newCoordinates, count: newCoordinates.count)
+      self.mapView.setVisibleMapRect(
+        self.mapView.mapRectThatFits(allPolyline.boundingMapRect),
+        edgePadding: UIEdgeInsets(top: 100, left: 50, bottom: 100, right: 50),
+        animated: false)
+    }
     
-    let lat1 = Double(coord1.latitude) * M_PI / 180
-    let lat2 = Double(coord2.latitude) * M_PI / 180
-    
-    let dLon = lon2 - lon1
-    
-    let x = cos(lat2) * cos(dLon)
-    let y = cos(lat2) * sin(dLon)
-    
-    let lat3 = atan2( sin(lat1) + sin(lat2), sqrt((cos(lat1) + x) * (cos(lat1) + x) + y * y) )
-    let lon3 = lon1 + atan2(y, cos(lat1) + x)
-    
-    return CLLocationCoordinate2D(latitude: lat3 * 180 / M_PI, longitude: lon3 * 180 / M_PI)
-  }
+    /**
+     * Find coordinate between two coordinates.
+     */
+    private func findCenterCoordinate(
+      coord1: CLLocationCoordinate2D, coord2: CLLocationCoordinate2D) -> CLLocationCoordinate2D {
+      
+      let lon1 = Double(coord1.longitude) * M_PI / 180
+      let lon2 = Double(coord2.longitude) * M_PI / 180
+      
+      let lat1 = Double(coord1.latitude) * M_PI / 180
+      let lat2 = Double(coord2.latitude) * M_PI / 180
+      
+      let dLon = lon2 - lon1
+      
+      let x = cos(lat2) * cos(dLon)
+      let y = cos(lat2) * sin(dLon)
+      
+      let lat3 = atan2( sin(lat1) + sin(lat2), sqrt((cos(lat1) + x) * (cos(lat1) + x) + y * y) )
+      let lon3 = lon1 + atan2(y, cos(lat1) + x)
+      
+      return CLLocationCoordinate2D(latitude: lat3 * 180 / M_PI, longitude: lon3 * 180 / M_PI)
+    }
 }
