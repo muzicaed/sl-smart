8bc9880f09e7dbcec6c60a997ea28e3ce0aa7b8c
Mikael Hellman <mikael.hellman@gmail.com>
2017-01-01 15:57:45 +0100

    Subscription UI fixes

--- Res Smart/Lib/SubscriptionStore.swift
+++ Res Smart/Lib/SubscriptionStore.swift
@@ -25,7 +25,7 @@ open class SubscriptionStore {
    */
   func isSubscribed() -> Bool {
     // TODO PAY: Remove this
-    return true
+    //return true
     
     loadSubscribedCache()
     return (isSubscribedCache! || isTrial())
@@ -36,7 +36,7 @@ open class SubscriptionStore {
    */
   func isTrial() -> Bool {
     // TODO PAY: Remove this
-    return false
+    //return false
     loadSubscribedCache()
     if !isSubscribedCache! {
       if let trialEndDate = defaults.object(forKey: TrialStartDate) as? Date {
