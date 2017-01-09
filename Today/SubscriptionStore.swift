45155c0b55bc2d5afd833a2b28ce829c4017f1dd
Mikael Hellman <mikael.hellman@gmail.com>
2017-01-09 09:28:52 +0100

    Turned on sub

--- Today/SubscriptionStore.swift
+++ Today/SubscriptionStore.swift
@@ -23,7 +23,10 @@ open class SubscriptionStore {
   /**
    * Check if user have a active subscription.
    */
-  func isSubscribed() -> Bool {    
+  func isSubscribed() -> Bool {
+    // TODO PAY: Remove this
+    return true
+    
     loadSubscribedCache()
     return (isSubscribedCache! || isTrial())
   }
@@ -32,6 +35,9 @@ open class SubscriptionStore {
    * Check if user have a active trial.
    */
   func isTrial() -> Bool {
+    // TODO PAY: Remove this
+    return false
+    
     loadSubscribedCache()
     if !isSubscribedCache! {
       if let trialEndDate = defaults.object(forKey: TrialStartDate) as? Date {
